423e1561634ea959b7db0a520f4ad81a2d6a904d
parksh5120-creator <parksh5120@gmail.com>
2026-05-19 21:44:20 +0900

    (2026.5.19) 애니메이션 시스템

---
+++ Program/Assets/Class 4th (Animation)/Scripts/Axe.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+   public void Attack(int count)
+    {
+        
+        
+    }
+    public void Depend()
+    {
+        Debug.Log("Depend");
+    }
+    public void Throw()
+    {
+        Debug.Log("Throw");
+    }
+}
