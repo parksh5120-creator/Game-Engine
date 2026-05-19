bbf53c800f8ecb892f0435199b136032156b2831
parksh5120-creator <parksh5120@gmail.com>
2026-05-19 17:29:37 +0900

    (2026.5.18) 수출용 패키지

---
+++ Program/Assets/Class 3th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Continue()
+    {
+        Debug.Log("Continue");
+    }
+    public void StartNewGame()
+    {
+        Debug.Log("New Game");
+    }
+    public void OpenOption()
+    {
+        Debug.Log("Option");
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
