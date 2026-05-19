07cf23bea48ca1d76f38ff9c6aa685ce46d487de
parksh5120-creator <parksh5120@gmail.com>
2026-05-19 15:56:03 +0900

    (2026.5.14) 스크립트

---
+++ Program/Assets/Class 2th (Script)/Scripts/Execution.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Execution : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Awake");
+    }
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+    private void Start()
+    {
+        Debug.Log("Start");
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log("Fixed Update");
+    }
+    private void Update()
+    {
+        Debug.Log("Update");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("Late Update");
+    }
+}
