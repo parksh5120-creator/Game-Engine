c6cc0bd5de09917be23c9d7a72d52f8fcc4ac2dd
parksh5120-creator <parksh5120@gmail.com>
2026-05-27 21:44:17 +0900

    (2026.5.27) 프리펩 시스템

--- Program/Assets/Class 6th (Input System)/Scripts/Character.cs
+++ Program/Assets/Class 6th (Input System)/Scripts/Character.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public Vector3 direction;
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
     void Update()
     {
-        direction.x = Input.GetAxis("Horizontal");
-        direction.z = Input.GetAxis("Vertical");
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
 
-        transform.position = transform.position + direction;
+        //Time.deltaTime은 이전 프레임과 현재 프레임 사이의 시간 간격을 나타냅니다.
+        direction.Normalize();
+
+        transform.position = transform.position + direction * speed * Time.deltaTime;
     }
 }
