59d228c79134bf0b6053f16387922c0f34cc44b4
parksh5120-creator <parksh5120@gmail.com>
2026-06-03 21:44:27 +0900

    (2026.6.3) 코루틴

--- Program/Assets/Class 9th (Coroutine)/Scripts/Create Manager.cs
+++ Program/Assets/Class 9th (Coroutine)/Scripts/Create Manager.cs
@@ -1,9 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateManager : MonoBehaviour
 {
     [SerializeField] float offset = 1.5f;
     [SerializeField] GameObject elemental;
+    [SerializeField] List<GameObject> list;
+    [SerializeField] int count;
+    [SerializeField] float time;
+
+
     void Start()
     {
         for (int i = 0; i < 5; i++)
@@ -11,7 +18,22 @@ public class CreateManager : MonoBehaviour
             GameObject clone = Instantiate(elemental, transform);
 
             clone.transform.position = new Vector3(-3 + (offset * i), 0, 0);
+
+            clone.SetActive(false);
+
+            list.Add(clone);
+
+            
         }
+        StartCoroutine(Activate());
+    }
+    IEnumerator Activate()
+    {
+        Debug.Log("Start Coroutine");
+
+        yield return new WaitForSeconds(5.0f);
+        
+        Debug.Log("Stop Coroutine");
     }
 
 }
